8309d4d0ecd5f2a07c437438878346017f906a44
Glitch (fantasy-relieved-velvet) <none>
2021-02-22 11:54:19 +0000

    🎲🎁 Checkpoint ./komutlar/yardımdeneme.js:20950640/1563

--- komutlar/yardımdeneme.js
+++ komutlar/yardımdeneme.js
@@ -1,23 +1,24 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 exports.run = function(client, message) {
-
-let botismi = 'Rolly Bot'
-const yardım = new Discord.MessageEmbed()
-.setColor('#010000')
-.addField(`${botismi} Yardım Menüsü`)
-.setDescription(`
+  let botismi = "Rolly Bot";
+  const yardım = new Discord.MessageEmbed()
+    .setColor("#010000")
+    .addField(`${botismi} Yardım Menüsü`)
+    .setDescription(
+      `
 r!moderasyon : moderasyon komutlarını gösterir
 r!eglence : eglence komutlarını gösterir
 r!uptime : uptime komutlarını gösterir
 r!koruma : koruma komutlarını gösterir
 r!kullanıcı : kullanıcı komutlarını gösterir
 r!müzik : müzik komutlarını gösterir
-vb. şekilde kafanıza göre yaparsınız burayı`)
-.setImage("")
-message.channel.send(yardım)
-}
-  exports.conf = {
+vb. şekilde kafanıza göre yaparsınız burayı`
+    )
+    .setImage("");
+  message.channel.send(yardım);
+};
+exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
@@ -25,7 +26,7 @@ message.channel.send(yardım)
 };
 
 exports.help = {
-  name: 'yardım',
-  description: 'Yardım Menüsünü Gösterir',
-  usage: '{prefix}yardım'
+  name: "yardım",
+  description: "Yardım Menüsünü Gösterir",
+  usage: "{prefix}yardım"
 };
